d832aba200b8d9f2ca5efe0ea5f78914136ec62d
Mezo-555 <144431907+Mezo-555@users.noreply.github.com>
2024-04-27 16:27:16 +0300

    Add files via upload

--- LibrarySystem/src/librarysystem/EBook.java
+++ LibrarySystem/src/librarysystem/EBook.java
@@ -1,14 +1,168 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package librarysystem;
 
-/**
- *
- * @author user
- */
-public class EBook {
+import java.util.Objects;
+
+public class EBook extends Book {
+
+    private String fileFormat;
+    private int fileSize;
+
+    public EBook(String fileFormat, int fileSize, String ISBN, String publisher, int publishYear, int numberOfPage, int edition, int id, String tittle, String category, Status s) {
+        super(ISBN, publisher, publishYear, numberOfPage, edition, id, tittle, category, s);
+        this.fileFormat = fileFormat;
+        this.fileSize = fileSize;
+    }
+
+    public EBook() {
+    }
+
+    @Override
+    public String getTittle() {
+        return super.getTittle();
+    }
+
+    @Override
+    public Status getS() {
+        return super.getS();
+    }
+
+    @Override
+    public String getPublisher() {
+        return super.getPublisher();
+    }
+
+    @Override
+    public int getPublishYear() {
+        return super.getPublishYear();
+    }
+
+    @Override
+    public int getNumberOfPage() {
+        return super.getNumberOfPage();
+    }
+
+    @Override
+    public int getId() {
+        return super.getId();
+    }
+
+    @Override
+    public String getISBN() {
+        return super.getISBN();
+    }
+
+    @Override
+    public int getEdition() {
+        return super.getEdition();
+    }
+
+    @Override
+    public String getCategory() {
+        return super.getCategory();
+    }
+
+    public String getFileFormat() {
+        return fileFormat;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    @Override
+    public void setTittle(String tittle) {
+        super.setTittle(tittle);
+    }
+
+    @Override
+    public void setS(Status s) {
+        super.setS(s);
+    }
+
+    @Override
+    public void setPublisher(String publisher) {
+        super.setPublisher(publisher);
+    }
+
+    @Override
+    public void setPublishYear(int publishYear) {
+        super.setPublishYear(publishYear);
+    }
+
+    @Override
+    public void setNumberOfPage(int numberOfPage) {
+        super.setNumberOfPage(numberOfPage);
+    }
+
+    @Override
+    public void setId(int id) {
+        super.setId(id);
+    }
+
+    @Override
+    public void setISBN(String ISBN) {
+        super.setISBN(ISBN);
+    }
+
+    @Override
+    public void setEdition(int edition) {
+        super.setEdition(edition);
+    }
+
+    @Override
+    public void setCategory(String category) {
+        super.setCategory(category);
+    }
+
+    public void setFileFormat(String fileFormat) {
+        this.fileFormat = fileFormat;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    @Override
+    public String toString() {
+        super.toString();
+        return "\nEBook{" + "fileFormat=" + fileFormat + ", fileSize=" + fileSize + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        super.equals(obj);
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final EBook other = (EBook) obj;
+        if (this.fileSize != other.fileSize) {
+            return false;
+        }
+        if (!Objects.equals(this.fileFormat, other.fileFormat)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void update(String item) {
+        super.update(item);/////TBD
+    }
+
+    @Override
+    public void remove(String item) {
+        super.remove(item);/////TBD
+    }
+
+    @Override
+    public void add(String item) {
+        super.add(item);/////TBD
+    }
     
 }
--- LibrarySystem/src/librarysystem/LibraryItem.java
+++ LibrarySystem/src/librarysystem/LibraryItem.java
@@ -1,14 +1,88 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package librarysystem;
 
-/**
- *
- * @author user
- */
-public interface LibraryItem {
+import java.util.Objects;
+
+public abstract class LibraryItem {
+    private int id;
+    private String tittle;
+    private String category;
+    private Status s;
+
+    public LibraryItem(int id, String tittle, String category, Status s) {
+        this.id = id;
+        this.tittle = tittle;
+        this.category = category;
+        this.s = s;
+    }
+
+    public LibraryItem() {
+    }
     
+    public int getId() {
+        return id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Status getS() {
+        return s;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setS(Status s) {
+        this.s = s;
+    }
+
+    @Override
+    public String toString() {
+        return "LibraryItem{" + "id=" + id + ", tittle=" + tittle + ", category=" + category + ", s=" + s + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LibraryItem other = (LibraryItem) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.tittle, other.tittle)) {
+            return false;
+        }
+        if (!Objects.equals(this.category, other.category)) {
+            return false;
+        }
+        if (this.s != other.s) {
+            return false;
+        }
+        return true;
+    }
+    
+    public abstract void add(String item);
+    public abstract void remove(String item);
+    public abstract void update(String item);
 }
