770dce530c98996cfb47f6710cf233d542868f59
Mezo-555 <144431907+Mezo-555@users.noreply.github.com>
2024-05-16 20:18:01 +0300

    Add files via upload

---
+++ src/librarysystem/EBook.java
@@ -0,0 +1,141 @@
+package librarysystem;
+
+import java.util.Objects;
+
+public class EBook extends Book {
+
+    private String fileFormat;
+    private int fileSize;
+    final String type = "E-Book";
+
+    public EBook(String fileFormat, int fileSize, String ISBN, String publisher, String author, int publishYear, int numberOfPage, int edition, String title, String category, Status s) {
+        super(ISBN, publisher, author, publishYear, numberOfPage, edition, title, category, s);
+        this.fileFormat = fileFormat;
+        this.fileSize = fileSize;
+    }
+
+    public EBook() {
+    }
+
+    @Override
+    public String getTitle() {
+        return super.getTitle();
+    }
+
+    @Override
+    public Status getS() {
+        return super.getS();
+    }
+
+    @Override
+    public String getPublisher() {
+        return super.getPublisher();
+    }
+
+    @Override
+    public int getPublishYear() {
+        return super.getPublishYear();
+    }
+
+    @Override
+    public int getNumberOfPage() {
+        return super.getNumberOfPage();
+    }
+
+    @Override
+    public int getId() {
+        return super.getId();
+    }
+
+    @Override
+    public String getISBN() {
+        return super.getISBN();
+    }
+
+    @Override
+    public int getEdition() {
+        return super.getEdition();
+    }
+
+    @Override
+    public String getCategory() {
+        return super.getCategory();
+    }
+
+    public String getFileFormat() {
+        return fileFormat;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        super.setTitle(title);
+    }
+
+    @Override
+    public void setS(Status s) {
+        super.setS(s);
+    }
+
+    @Override
+    public void setPublisher(String publisher) {
+        super.setPublisher(publisher);
+    }
+
+    @Override
+    public void setPublishYear(int publishYear) {
+        super.setPublishYear(publishYear);
+    }
+
+    @Override
+    public void setNumberOfPage(int numberOfPage) {
+        super.setNumberOfPage(numberOfPage);
+    }
+
+    @Override
+    public void setISBN(String ISBN) {
+        super.setISBN(ISBN);
+    }
+
+    @Override
+    public void setEdition(int edition) {
+        super.setEdition(edition);
+    }
+
+    @Override
+    public void setCategory(String category) {
+        super.setCategory(category);
+    }
+
+    public void setFileFormat(String fileFormat) {
+        this.fileFormat = fileFormat;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    @Override
+    public String toString() {
+        super.toString();
+        return "\nEBook{" + "fileFormat=" + fileFormat + ", fileSize=" + fileSize + '}';
+    }
+
+    // equals method
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof EBook)) {
+            return false;
+        }
+        EBook ebook = (EBook) obj;
+        return super.equals(obj) &&
+                Objects.equals(fileFormat, ebook.fileFormat) &&
+                Double.compare(ebook.fileSize, fileSize) == 0;
+    }
+}
---
+++ src/librarysystem/LibrarySystem.java
@@ -0,0 +1,36 @@
+package librarysystem;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class LibrarySystem {
+
+   // public static void main(String[] args) {
+        //LibraryActions ac = new LibraryActions();
+        //Librarian l = new Librarian();
+        //User user = l.getUser(2);
+        //user.updateDetails("Samir", "9101", "Home3");
+        //User.register("Moataz", "1234", "Home");
+        //LibraryItem item = new Book("978-0-13-468599-1", "Pearson", "Robert Sedgewick", 2019, 784, 4, "Algorithms", "Computer Science", Status.AVAILABLE);
+        //Date dueDate = generateRandomDate();
+        //l.addLibraryItem(item);
+        //item.setAuthor("Hobo");
+        //l.updateLibraryItem(item);
+        //l.removeLibraryItem(item);
+        //ac.lendItem(1, 1, dueDate);
+        //ac.returnItem(1);
+        //ac.searchLibraryItems("222");
+        //l.displayLibraryItems();
+        //l.displayUsers();
+        //l.manageUser(, "update");
+        //l.getUser(1);
+        //l.trackBorrowings();
+    }
+    // Method to generate a random date
+   // private static Date generateRandomDate() {
+   //     long offset = Timestamp.valueOf("2022-01-01 00:00:00").getTime();
+   //     long end = Timestamp.valueOf("2025-01-01 00:00:00").getTime();
+   //     long diff = end - offset + 1;
+   //     return new Date(offset + (long)(Math.random() * diff));
+   // }
+//}
